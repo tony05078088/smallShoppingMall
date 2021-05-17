8807f377ab33ac10abc67dd5d4269bc6e8b50e15
William <tony05078088@gmail.com>
2021-05-17 11:48:18 +0800

    接口錯誤攔截

--- src/main.js
+++ src/main.js
@@ -2,8 +2,26 @@ import axios from "axios";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+// 掛載axios到vue實例上
 Vue.prototype.axios = axios;
 
+//根據前端跨域方式做調整 Ex: /a/b  :  /api/a/b => /a/b
+axios.defaults.baseURL = "/api";
+axios.defaults.timeout = 8000
+//axios 接口錯誤攔截器
+axios.interceptors.response.use(response => {
+  let res = response.data;
+  if (res.status == 0) {
+    return res.data;
+  } else if (res.status == 10) {
+    //未登入
+    window.location.href = "/#/login";
+  } else {
+    alert(res.msg);
+  }
+});
+
+//生產環境提示
 Vue.config.productionTip = false;
 new Vue({
   router,
