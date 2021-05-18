659f8a04e0222367e7f6c3e723de8cc15040cda4
William <tony05078088@gmail.com>
2021-05-18 11:53:06 +0800

    sass-loader

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    nav-header
+  <div class="header">
+    <div class="nav-topbar">
+      <div class="container">
+        <div class="topbar-menu">
+          <a href="javascript:;">小米商城</a>
+          <a href="javascript:;">MUI</a>
+          <a href="javascript:;">雲服務</a>
+          <a href="javascript:;">協議規則</a>
+        </div>
+        <div class="topbar-user">
+          <a href="javascript:;">登錄</a>
+          <a href="javascript:;">註冊</a>
+          <a href="javascript:;">購物車</a>
+        </div>
+      </div>
+    </div>
+    <div class="nav-header"></div>
   </div>
 </template>
 
@@ -10,4 +25,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.header {
+  .container {
+    width: 1226px;
+    margin:0 auto;
+  }
+}
+</style>
