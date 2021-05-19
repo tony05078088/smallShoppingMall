d48697ae1a2f441d78d4f05451cf56b15b422296
William <tony05078088@gmail.com>
2021-05-19 17:01:48 +0800

    NavFooter組件

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="" />
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="" target="_blank">河畔一角主頁</a><span>|</span>
+      <a href="" target="_blank">Vue全端課程</a><span>|</span>
+      <a href="" target="_blank">React全家桶</a><span>|</span>
+      <a href="" target="_blank">微信支付</a>
+    </div>
+    <div class="CopyRight">
+      Copyright ©2020 <span class="domain">mi.futurefe.com </span> All Rights
+      Reserved.
+    </div>
   </div>
 </template>
 
@@ -10,4 +23,37 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import "./../assets/scss/config.scss";
+.footer {
+  height: 234px;
+  border-top: 4px solid $colorA;
+  background: $colorB;
+  color: $colorD;
+  font-size: $fontI;
+  text-align: center;
+  .footer-logo {
+    margin: 46px 0 31px 0;
+    img {
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link {
+    a {
+      color: $colorD;
+      display: inline-block;
+    }
+    span {
+      margin: 0 10px;
+    }
+  }
+  margin-bottom: 13px;
+  .CopyRight {
+    .domain {
+      color: $colorA;
+    }
+  }
+}
+</style>
